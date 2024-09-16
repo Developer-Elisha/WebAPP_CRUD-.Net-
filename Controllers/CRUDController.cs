2a9adcd12dcb0c417f06b7a46fe856d45b12687d
Developer-Elisha <developer.elisha@gmail.com>
2024-09-17 04:15:52 +0500

    Full CRUD on Asp.Net Mvc Core

---
+++ Controllers/CRUDController.cs
@@ -0,0 +1,87 @@
+﻿using CRUD_AspDotNet.DB_Context;
+using CRUD_AspDotNet.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace CRUD_AspDotNet.Controllers
+{
+    public class CRUDController : Controller
+    {
+        SqlContext sc;
+        public CRUDController(SqlContext sc1)
+        {
+            this.sc = sc1;
+        }
+
+        public IActionResult add_user()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult add_user(User us)
+        {
+            if (us == null || string.IsNullOrEmpty(us.Name) || string.IsNullOrEmpty(us.LastName))
+            {
+                ModelState.AddModelError(string.Empty, "Name and LastName are required.");
+                return View(us);
+            }
+            sc.Add(us);
+            sc.SaveChanges();
+            ModelState.Clear();
+            return RedirectToAction("FetchUser");
+        }
+
+        public IActionResult FetchUser()
+        {
+            var us = sc.tblusers.ToList();
+            return View(us);
+        }
+
+        public IActionResult deleteuser(int id)
+        {
+            var us = sc.tblusers.Find(id);
+            if (us == null)
+            {
+                return RedirectToAction("error404");
+            }
+
+            sc.tblusers.Remove(us);
+            sc.SaveChanges();
+            return RedirectToAction("FetchUser");
+        }
+
+        public IActionResult edit_user(int id)
+        {
+            var us = sc.tblusers.Find(id);
+            if (us == null)
+            {
+                return RedirectToAction("error404");
+            }
+            return View(us);
+        }
+
+        [HttpPost]
+        public IActionResult edit_user(User user)
+        {
+            if (user == null || user.Id <= 0 || string.IsNullOrEmpty(user.Name) || string.IsNullOrEmpty(user.LastName))
+            {
+                ModelState.AddModelError(string.Empty, "Invalid data.");
+                return View(user);
+            }
+
+            var existingUser = sc.tblusers.Find(user.Id);
+            if (existingUser == null)
+            {
+                return RedirectToAction("error404");
+            }
+
+            existingUser.Name = user.Name;
+            existingUser.LastName = user.LastName;
+            // Update other fields as needed
+
+            sc.SaveChanges();
+            return RedirectToAction("FetchUser");
+        }
+    }
+}
---
+++ DB Context/SqlContext.cs
@@ -0,0 +1,14 @@
+﻿using CRUD_AspDotNet.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_AspDotNet.DB_Context
+{
+    public class SqlContext : DbContext
+   {
+      public SqlContext(DbContextOptions<SqlContext> option) : base(option)
+    {
+
+    }
+    public DbSet<User> tblusers { get; set; }
+}
+}
